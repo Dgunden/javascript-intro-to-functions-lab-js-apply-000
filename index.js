b2e52ee5360cababfc96ff530e96524021e8fed7
Dennis Gunden <dennisgunden@gmail.com>
2019-12-03 15:45:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function sayHiToGrandma(string) {
  } 
   
 function sayHiToGrandma(string) {
-  if(string === "I love you, Grandma.") {
+  if("I love you, Grandma.") {
     return "I love you, too.";
   }
 }
